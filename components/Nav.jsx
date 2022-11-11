67abb74ae13dc0b86716850c11a4e4e482fe6127
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-11-11 20:32:41 +0100

    Implement Nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,39 @@
+import Link from 'next/link';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../utils/firebase';
+
 export default function Nav() {
+  const [user, loading] = useAuthState(auth);
+
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav className="flex justify-between items-center py-10">
+      <Link href="/">
+        <button className="text-lg font-medium">Genius Ideas</button>
+      </Link>
+      <ul className="flex items-center gap-10">
+        {!user && (
+          <Link href={'/auth/login'}>
+            <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+              Join Now
+            </a>
+          </Link>
+        )}
+        {user && (
+          <div className="flex items-center gap-6">
+            <Link href="/post">
+              <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-lg text-sm">
+                Post
+              </button>
+            </Link>
+            <Link href="/dashboard">
+              <img
+                className="w-12 rounded-full cursor-pointer"
+                src={user.photoURL}
+              />
+            </Link>
+          </div>
+        )}
+      </ul>
+    </nav>
   );
 }
