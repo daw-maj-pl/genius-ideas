5cccde24ecc1c0d443de8c594a925ee657718638
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-11-14 20:12:33 +0100

    Hook up react-toastify

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <ToastContainer limit={1} />
       <Component {...pageProps} />
     </Layout>
   );
