6e5a828dc18c66450da5a2b71a9da5c8d6f94b43
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-11-15 19:31:08 +0100

    Fetch and render all posts

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '../utils/firebase';
+import Message from '../components/Message';
 
 export default function Home() {
+  const [allPosts, setAllPosts] = useState([]);
+
+  const getPosts = async () => {
+    const postsRef = collection(db, 'posts');
+    const q = query(postsRef, orderBy('timestamp', 'desc'));
+    const unsubscribe = onSnapshot(q, snapshot => {
+      setAllPosts(snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+    });
+    return unsubscribe;
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div>
       <Head>
@@ -8,7 +27,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <div className="my-12 text-lg font-medium">
+        <h2>See other people ideas</h2>
+        {allPosts.map(post => (
+          <Message key={post.id} {...post}></Message>
+        ))}
+      </div>
     </div>
   );
 }
