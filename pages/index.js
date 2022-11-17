bea77e5f27313ce4739c5d485c15aa0f0c232038
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-11-17 20:14:32 +0100

    Add link to comments and fix bug with async

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 import { db } from '../utils/firebase';
 import Message from '../components/Message';
@@ -7,7 +8,7 @@ import Message from '../components/Message';
 export default function Home() {
   const [allPosts, setAllPosts] = useState([]);
 
-  const getPosts = async () => {
+  const getPosts = () => {
     const postsRef = collection(db, 'posts');
     const q = query(postsRef, orderBy('timestamp', 'desc'));
     const unsubscribe = onSnapshot(q, snapshot => {
@@ -17,7 +18,7 @@ export default function Home() {
   };
 
   useEffect(() => {
-    getPosts();
+    return getPosts();
   }, []);
 
   return (
@@ -30,7 +31,14 @@ export default function Home() {
       <div className="my-12 text-lg font-medium">
         <h2>See other people ideas</h2>
         {allPosts.map(post => (
-          <Message key={post.id} {...post}></Message>
+          <Message key={post.id} {...post}>
+            <Link href={{ pathname: `/${post.id}`, query: { ...post } }}>
+              <button>
+                {/* {post.comments?.length > 0 ? post.comments?.length : 0} comments */}
+                {post.comments?.length || 0} comments
+              </button>
+            </Link>
+          </Message>
         ))}
       </div>
     </div>
